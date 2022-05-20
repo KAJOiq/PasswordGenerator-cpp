0293562b612472004cd8a0eb7f74470eb3fafc5d
Abdulrahman Ahmed S <96299823+KAJOiq@users.noreply.github.com>
2022-05-20 20:22:35 +0300

    Create passwordgenerator.cpp

---
+++ passwordgenerator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// تستخدم هذه المكتبة لتحديد خصائص الذاكرة الديناميكية وتوليد العشوائية
+#include <cstdlib>
+
+// لتضمين الوقت منذُ نشوء اللغة الى الآن
+#include <ctime>
+using namespace std;
+
+static const char pass[] = 
+"0123456789"
+"!@#$%^&*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+
+int size = sizeof(pass) - 1;
+
+int main() 
+{
+  // password length 
+  int length_pass;
+  cout << "Password length --> ";
+  cin >> length_pass;
+  // to help in the generation of random numbers by seeding rand with a starting value
+  srand(time(0));
+  cout << "Password suggestion for you --> ";
+  for(int i=0 ; i<length_pass ; i++)
+    {
+      cout << pass[rand() % :: size];
+    }
+  return 0;
+}
